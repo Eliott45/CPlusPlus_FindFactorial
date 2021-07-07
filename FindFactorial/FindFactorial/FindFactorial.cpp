b495c43c27e9d58839438f0d112aafcd5920acf6
Eliott <eliott_nu@mail.ru>
2021-07-07 19:51:48 +0900

    The solution of the problem

--- FindFactorial/FindFactorial/FindFactorial.cpp
+++ FindFactorial/FindFactorial/FindFactorial.cpp
@@ -2,11 +2,23 @@
 
 using namespace std;
 
+/// <summary>
+/// Finding the factorial.
+/// </summary>
+/// <param name="n">Non-negative integer n.</param>
+/// <returns>Product of all positive integers less than or equal to n.</returns>
+long long factorial(int n) {
+    return n == 0 ? 1 : n * factorial(n - 1);
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    int num;
+    cout << "Write a number:";
+    cin >> num;
+
+    cout << factorial(num);
 
     return 0;
 }
-
 
